c94ad66165bc8a6eef608de550b1e353e7cd6ecd
Bastian Allgeier <mail@bastianallgeier.com>
2022-11-23 14:52:45 +0100

    Add a new bootstrapper to load the index.php from different public folders

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,4 +6,5 @@ ini_set('memory_limit', '512M');
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
+require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . '/../vendor/autoload.php';

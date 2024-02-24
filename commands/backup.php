6dec129a559d7f45e3750a2a634a13fafacae85a
Ahmet Bora <byybora@gmail.com>
2024-02-24 14:00:27 +0300

    Switch to php native methods

--- commands/backup.php
+++ commands/backup.php
@@ -3,8 +3,6 @@
 declare(strict_types = 1);
 
 use Kirby\CLI\CLI;
-use Kirby\Filesystem\Dir;
-use Kirby\Toolkit\Str;
 
 return [
 	'description' => 'Creates backup of application files',
@@ -14,44 +12,60 @@ return [
 		]
 	],
 	'command' => static function (CLI $cli): void {
+		if (class_exists('ZipArchive') === false) {
+			throw new Exception('ZipArchive library could not be found');
+		}
+
 		$root       = $cli->argOrPrompt(
 			'root',
 			'Which root should be backup? (press <Enter> to backup the entire kirby application)',
 			false
 		);
 		$root 	    = empty($root) === true ? 'index' : $root;
-		$targetPath = $cli->kirby()->root($root);
+		$rootPath   = $cli->kirby()->root($root);
 
-		if ($targetPath === null) {
+		if ($rootPath === null) {
 			throw new Exception('Invalid root entered: ' . $root);
 		}
 
-		$kirbyPath          = $cli->kirby()->root('index');
-		$backupPath         = $kirbyPath . '/backup';
-		$backupFile         = $backupPath . '/' . $root . '-' . date('Y-m-d-His') . '.zip';
-		$relativeBackupFile = Str::after($backupFile, $kirbyPath);
-		$relativeTargetPath = trim(Str::after($targetPath, $kirbyPath), '/');
-
-		// execution commands list
-		$commands = [
-			// navigates to the target directory to ignore parent folders in zip file
-			'cd ' . escapeshellarg($targetPath) . ';',
-			// sets backup file path
-			'zip -r ' . escapeshellarg($backupFile),
-			// sets target backup directory
-			escapeshellarg($root === 'index' ? '*' : ('./' . $relativeTargetPath . '/*'))
-		];
-
-		// exclude backup directory from the root for only index root
-		if ($root === 'index') {
-			$commands[] = '-x ' . escapeshellarg('backup/*');
-		}
+		$kirbyPath  = $cli->kirby()->root('index');
+		$backupPath = $kirbyPath . '/backup';
+		$backupFile = $backupPath . '/' . $root . '-' . date('Y-m-d-His') . '.zip';
 
 		// create backup directory before the process
-		Dir::make($backupPath);
+		mkdir($backupPath);
 
-		exec(implode(' ', $commands));
+		$zip = new ZipArchive();
+		if ($zip->open($backupFile, ZipArchive::CREATE) !== true) {
+			throw new Exception('Failed to create backup file');
+		}
 
-		$cli->success('The backup has been created: ' . $relativeBackupFile);
+		$files = new RecursiveIteratorIterator(
+			new RecursiveCallbackFilterIterator(
+				new RecursiveDirectoryIterator(
+					$rootPath,
+					FilesystemIterator::SKIP_DOTS
+				),
+				fn ($file) => $file->isFile() || in_array($file->getBaseName(), ['.git', 'backup']) === false
+			)
+		);
+
+		foreach ($files as $file) {
+			// skip directories, will be added automatically
+			if ($file->isDir() === false) {
+				// get real and relative path for current file
+				$filePath = $file->getRealPath();
+				$relativePath = substr($filePath, strlen($rootPath) + 1);
+
+				// add current file to archive
+				$zip->addFile($filePath, $relativePath);
+			}
+		}
+
+		if ($zip->close() === false) {
+			throw new Exception('There was a problem writing the backup file');
+		}
+
+		$cli->success('The backup has been created: ' . substr($backupFile, strlen($kirbyPath)));
 	}
 ];

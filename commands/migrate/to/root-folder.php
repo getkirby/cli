592f4ab2217d7b0f6311c4f9ea4136335d127102
Lukas Bestle <lukas@getkirby.com>
2025-10-11 21:45:29 +0200

    fix: Typo in command description

--- commands/migrate/to/root-folder.php
+++ commands/migrate/to/root-folder.php
@@ -5,6 +5,6 @@ declare(strict_types = 1);
 use Kirby\CLI\Commands\Migrate\To\RootFolder;
 
 return [
-	'description' => 'Switch to a rooter folder setup',
+	'description' => 'Switch to a root folder setup',
 	'command'     => RootFolder::command(...)
 ];
